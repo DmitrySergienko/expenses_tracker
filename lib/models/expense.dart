01bc2efc6bc7867acfd83fb7c32fed2f86833a88
DmitrySergienko <dmitrisergienko@gmail.com>
2023-07-04 11:57:03 +0400

    list updated (enum category icon)

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, work, leisure }
+
+const categoryIcons ={
+  Category.food: Icons.lunch_dining_outlined,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+
+};
 
 class Expense {
   Expense(
@@ -14,7 +26,12 @@ class Expense {
 
   final String id;
   final String title;
-  final String amount;
+  final double amount;
   final DateTime date;
-  final String categoty;
+  final Category categoty;
+
+  String get formattedDate{
+return formatter.format(date);
+  }
+
 }
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expenses_tracker/widgets/expenses_list.dart/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registredExpences = [
+    Expense(
+        title: 'Flutter course',
+        amount: 19.9,
+        date: DateTime.now(),
+        categoty: Category.leisure),
+    Expense(
+        title: 'Dummy exp',
+        amount: 19.9,
+        date: DateTime.now(),
+        categoty: Category.work),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        const Text('Some text'),
+        // to show list on the screen, the list should be inside Expanded() function
+        Expanded(
+          child: ExpensesList(expenses: _registredExpences),
+        ),
+      ]),
+    );
+  }
+}
